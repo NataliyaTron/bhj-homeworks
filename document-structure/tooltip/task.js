a2c88beab189d70d03ba4acc544886bb54551c79
NataliyaTron <118108299+NataliyaTron@users.noreply.github.com>
2023-05-01 21:49:42 +0400

    first commit tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const links = Array.from(document.getElementsByClassName(".has-tooltip"));
+
+function makeTooltipActive(event) {
+  event.preventDefault();
+  let deletableElement = document.querySelector(".tooltip tooltip_active")[0];
+  if (deletableElement) {
+    deletableElement.remove();
+  }
+  let tooltipText = this.title;
+  this.insertAdjacentHTML(
+    "afterend",
+    <div class="tooltip tooltip_active">`${tooltipText}`</div>
+  );
+}
+
+for (let i = 0; i < links.length; i++) {
+  const link = links[i];
+  link.addEventListener("click", makeTooltipActive);
+}
