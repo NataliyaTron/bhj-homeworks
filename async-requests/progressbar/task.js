7b09ee8e5047a235267f493994c6d5cf2e9878ab
NataliyaTron <118108299+NataliyaTron@users.noreply.github.com>
2023-05-08 14:31:19 +0400

    first commit progressbar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,22 @@
+"use strict";
+const submitFile = document.getElementById("form");
+const progress = document.getElementById("progress");
+let i = 0;
+
+function submitForm(e) {
+  e.preventDefault();
+  const form = new FormData(submitFile);
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
+  xhr.addEventListener("progress", loaderForm);
+  xhr.send(form);
+}
+
+function loaderForm() {
+  if (this.status === 200) {
+    i = i + 1;
+    progress.value = i / 20;
+  }
+}
+
+form.addEventListener("submit", submitForm);
